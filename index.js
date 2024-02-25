d8b198d4538b82b94d368668b34678e19c23a619
Alexandr Demidov <alex.demidov.2015@gmail.com>
2024-02-26 02:39:52 +0500

    Now compute float values

--- index.js
+++ index.js
@@ -1,21 +1,25 @@
+const OPERATORS = ['+', '-', '*', '/'];
+
 let operands = [];
 let operator = '';
 
 function onButtonClick(value) {
     const display = document.getElementById("display");
-    const synbolEq = value === "=";
+    const symbolEq = value === "=";
     const symbolNumber = !isNaN(+value);
+    const symbolIsOperator = OPERATORS.includes(value);
+    const dota = value === ".";
+    
+    if (symbolNumber || dota) display.innerText += value;
 
-    if (symbolNumber) display.innerText += value;
 
-
-    if (!symbolNumber && !synbolEq && operator === "") {
+    if ( operator === "" && symbolIsOperator) {
         operator = value;
         display.innerText += value;
         return;
     }
 
-    if (!symbolNumber && !synbolEq && operator !== "") {
+    if ( operator !== "" && symbolIsOperator) {
         const result = calculate(display);
         operands = [];
         operator = value;
@@ -23,16 +27,17 @@ function onButtonClick(value) {
         return;
     }
 
-    if (value === "=") {
+    if (symbolEq) {
         const result = calculate(display);
         operands = [];
         display.innerText = result;
         operator = "";
+        return;
     }
 }
 
 function calculate(display) {
-    operands = display.innerText.split(/[^\d]/).filter(e => e !== "").map(e => +e);
+    operands = display.innerText.split(/[^\d\.]/).filter(e => e !== "").map(e => +e);
     return doOperation();
 }
 
